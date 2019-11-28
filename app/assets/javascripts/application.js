1c04046cd6d6f552557ddb5fd1e5ba42fb43a74b
Cesar Corregiari <cesarcorregiari@gmail.com>
2019-11-28 15:23:16 -0300

    search bar home v2

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,8 +1,18 @@
-//= require rails-ujs
 //= require jquery
-//= require_tree .
+//= require rails-ujs
 //= require select2
+//= require_tree .
 
-$(document).ready(function() {
-	$('.origin-search-form').select2()
+$(document).ready(function(){
+	$('.origin-search-form').select2({
+		placeholder: 'Origem',
+		allowClear: true,
+		width: 100,
+	})
+	$('.destino-search-form').select2({
+		placeholder: 'Destino',
+		allowClear: true,
+		width: 100
+	})
 })
+
