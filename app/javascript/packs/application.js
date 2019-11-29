30b7dc2a7609291753d7cf1f0dd2e5cff669fd48
Camilla Kunii <milla.mk@hotmail.com>
2019-11-29 17:35:43 -0300

    update gems

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,9 +2,9 @@ import "bootstrap";
 import "select2.css";
 import "select2-bootstrap.css";
 
-// import flatpickr from 'flatpickr'
-// import 'flatpickr/dist/flatpickr.min.css'
+import flatpickr from 'flatpickr'
+import 'flatpickr/dist/flatpickr.min.css'
 
-// flatpickr(".datepicker", {
+flatpickr(".datepicker", {
 
-// });
+});
