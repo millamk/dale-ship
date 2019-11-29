baa7dbad79fc2bc099794c815f7b338b4d666b5f
Camilla Kunii <milla.mk@hotmail.com>
2019-11-29 17:51:13 -0300

    select2 theme add

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,6 +1,6 @@
 import "bootstrap";
 // import "select2";
-import "select2-bootstrap";
+import "select2-bootstrap-theme";
 
 import flatpickr from 'flatpickr'
 import 'flatpickr/dist/flatpickr.min.css'
