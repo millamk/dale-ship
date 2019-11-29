201eeaa3ba68120f77ff814d24f6cca1c8b688ff
Camilla Kunii <milla.mk@hotmail.com>
2019-11-29 17:03:22 -0300

    comment select css

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,26 +1,26 @@
 //= require jquery
 //= require rails-ujs
-// = require select2
+//= require select2
 //= require_tree .
 
-// $(document).ready(function(){
-// 	$('.origin-search-form').select2({
-// 		placeholder: 'Origem',
-// 		allowClear: true,
-// 		theme: "bootstrap",
-// 		width: 100,
-// 	})
-// 	$('.destino-search-form').select2({
-// 		placeholder: 'Destino',
-// 		allowClear: true,
-// 		theme: "bootstrap",
-// 		width: 100
-// 	})
-// 	$('#type_of_shipment').select2({
-// 		placeholder: ' ',
-// 		allowClear: true,
-// 		theme: "bootstrap",
-// 		width: 100
-// 	})
-// })
+$(document).ready(function(){
+	$('.origin-search-form').select2({
+		placeholder: 'Origem',
+		allowClear: true,
+		theme: "bootstrap",
+		width: 100,
+	})
+	$('.destino-search-form').select2({
+		placeholder: 'Destino',
+		allowClear: true,
+		theme: "bootstrap",
+		width: 100
+	})
+	$('#type_of_shipment').select2({
+		placeholder: ' ',
+		allowClear: true,
+		theme: "bootstrap",
+		width: 100
+	})
+})
 
