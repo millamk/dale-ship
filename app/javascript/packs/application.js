1620b6359a8fce5b3b06d9da3964af62d14ca46d
Camilla Kunii <milla.mk@hotmail.com>
2019-11-29 17:49:01 -0300

    select2 add

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,6 +1,6 @@
 import "bootstrap";
-import "select2.css";
-import "select2-bootstrap.css";
+import "select2";
+import "select2-bootstrap";
 
 import flatpickr from 'flatpickr'
 import 'flatpickr/dist/flatpickr.min.css'
