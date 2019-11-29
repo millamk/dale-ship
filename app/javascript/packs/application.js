f111af7160c3ece6155b5aad0a56036750cd2dec
Camilla Kunii <milla.mk@hotmail.com>
2019-11-29 17:45:53 -0300

    bootstrap e flat

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,10 +1,10 @@
-// import "bootstrap";
+import "bootstrap";
 // import "select2.css";
 // import "select2-bootstrap.css";
 
-// import flatpickr from 'flatpickr'
-// import 'flatpickr/dist/flatpickr.min.css'
+import flatpickr from 'flatpickr'
+import 'flatpickr/dist/flatpickr.min.css'
 
-// flatpickr(".datepicker", {
+flatpickr(".datepicker", {
 
-// });
+});
