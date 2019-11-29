3aa12c9228cfb73af307d25769741483903e36ad
Camilla Kunii <milla.mk@hotmail.com>
2019-11-29 13:32:48 -0300

    ajuda do Vini para fazer o default do booking como pending

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -3,6 +3,7 @@ class BookingsController < ApplicationController
     @booking = Booking.new
     @booking.shipper = current_user.shipper
     @booking.freight = Freight.find(params[:freight_id])
+    @booking.status = 'Pending'
     @booking.save
     redirect_to shippers_booking_path(@booking)
   end
--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,5 +2,5 @@ class Booking < ApplicationRecord
   belongs_to :shipper
   belongs_to :freight
 
-  enum status: { pending: 'Pending', approved: 'Approved', rejected: 'Rejected' }
+  enum status: { approved: 'Approved', rejected: 'Rejected' }
 end
