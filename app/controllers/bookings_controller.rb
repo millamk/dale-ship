09e970a38826c1260e2c63f2aaf98ad9eebe504f
Camilla Kunii <milla.mk@hotmail.com>
2019-12-02 08:47:50 -0300

    try to fix

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,15 +1,15 @@
 class BookingsController < ApplicationController
   def create
-    unless Freight.find(params[:freight_id]).shippers.count > 0
-      @booking = Booking.new
-      @booking.shipper = current_user.shipper
-      @booking.freight = Freight.find(params[:freight_id])
-      @booking.status = 'Pending'
-      @booking.save
-      redirect_to shippers_booking_path(@booking)
-    else
-      flash[:alert] = "We're sorry to inform that this freight is no longer available"
-      redirect_to root_path
-    end
+    # unless Freight.find(params[:freight_id]).shippers.count > 0
+    @booking = Booking.new
+    @booking.shipper = current_user.shipper
+    @booking.freight = Freight.find(params[:freight_id])
+    @booking.status = 'Pending'
+    @booking.save
+    redirect_to shippers_booking_path(@booking)
+    # else
+    #   flash[:alert] = "We're sorry to inform that this freight is no longer available"
+    #   redirect_to root_path
+    # end
   end
 end
