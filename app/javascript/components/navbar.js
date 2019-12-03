76605069120f975ee88dd82e9ffeed6d4ebf59c2
Camilla Kunii <milla.mk@hotmail.com>
2019-12-03 12:13:42 -0300

    2 navbars

--- app/javascript/components/navbar.js
+++ app/javascript/components/navbar.js
@@ -1,11 +1,11 @@
 const initUpdateNavbarOnScroll = () => {
-  const navbar = document.querySelector('.navbar-lewagon');
+  const navbar = document.querySelector('.navbar-banner');
   if (navbar) {
     window.addEventListener('scroll', () => {
       if (window.scrollY >= 530) {
-        navbar.classList.add('navbar-lewagon-white');
+        navbar.classList.add('navbar-banner-white');
       } else {
-        navbar.classList.remove('navbar-lewagon-white');
+        navbar.classList.remove('navbar-banner-white');
       }
     });
   }
