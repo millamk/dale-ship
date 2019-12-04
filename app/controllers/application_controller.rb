8bf88dae63ed4224d0da5e48fd7831fe7e786708
Camilla Kunii <milla.mk@hotmail.com>
2019-12-04 18:39:24 -0300

    Add protect_from_forgery

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :dispatch_user
+  protect_from_forgery prepend: true
   add_flash_types :full
 
   def dispatch_user
