b5d936cf48f800f81ac18bdc8b38be4b9254b22f
Camilla Kunii <milla.mk@hotmail.com>
2019-12-04 18:52:12 -0300

    Add config.domain

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,4 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery prepend: true
   before_action :authenticate_user!
   before_action :dispatch_user
   add_flash_types :full
