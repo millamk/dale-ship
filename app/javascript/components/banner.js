2abd67c5f9e9623a84df7f55bef302de4db9f488
Camilla Kunii <milla.mk@hotmail.com>
2019-12-06 18:35:20 -0300

    ajustado a palavra anywhere

--- app/javascript/components/banner.js
+++ app/javascript/components/banner.js
@@ -2,7 +2,7 @@ import Typed from 'typed.js';
 
 const loadDynamicBannerText = () => {
   new Typed('#banner-typed-text', {
-    strings: ["Unique trading experience", "Shipping to and from anywhrere"],
+    strings: ["Unique trading experience", "Shipping to and from anywhere"],
     typeSpeed: 50,
     loop: true
   });
